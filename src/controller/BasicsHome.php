d54ad2224c37e0fd125c059dae9988b14704f018
pizepei <848733913@qq.com>
2019-10-15 23:41:26 +0800

    设计支持自定义layuiadmin配置功能

--- src/controller/BasicsHome.php
+++ src/controller/BasicsHome.php
@@ -3,19 +3,43 @@
  * 首页控制器
  */
 namespace pizepei\basics\controller;
+use pizepei\basics\service\layuiadmin\BasicsLayuiAdminService;
 use pizepei\staging\Controller;
 
 class BasicsHome extends Controller
 {
     /**
      * @return array [html]
-     * @title  / 路由的应用
+     * @title  / 默认首页
      * @explain 注意所有 path 路由都使用 正则表达式为唯一凭证 所以 / 路由只能有一个
      * @baseAuth UserAuth:public
      * @router get /index.html
      */
     public function index()
     {
-        return $this->view();
+        return (new BasicsLayuiAdminService())->getIndexHtml('sss');
+    }
+
+    /**
+     * @return array [js]
+     * @title  / 默认首页
+     * @explain 注意所有 path 路由都使用 正则表达式为唯一凭证 所以 / 路由只能有一个
+     * @baseAuth UserAuth:public
+     * @router get config.js
+     */
+    public function homeConfig()
+    {
+        return (new BasicsLayuiAdminService())->getConfig('sss');
+    }
+    /**
+     * @return array [js]
+     * @title  / 默认首页
+     * @explain 注意所有 path 路由都使用 正则表达式为唯一凭证 所以 / 路由只能有一个
+     * @baseAuth UserAuth:public
+     * @router get index.js
+     */
+    public function homeIndex()
+    {
+        return (new BasicsLayuiAdminService())->getIndexJs('sss');
     }
 }
---
+++ src/service/layuiadmin/BasicsLayuiAdminService.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Class BasicsLayuiAdminService
+ * layuiadmin 处理服务
+ */
+namespace pizepei\basics\service\layuiadmin;
+/**
+ * Class BasicsLayuiAdminService
+ * @package pizepei\basics\service\layuiadmin
+ */
+class BasicsLayuiAdminService
+{
+    /**
+     * 获取首页index
+     * @param string $domain
+     * @return string
+     */
+    public function getIndexHtml(string $domain):string
+    {
+        $data = [
+            'title'=>'Lifestyle',
+            'index'=>'../../normative//home/index',
+            'base'=>'./resource/src/',
+            'css'=>'./resource/start/layui/css/layui.css',
+            'js'=>'./resource/start/layui/layui.js',
+        ];
+        $file = file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'template'.DIRECTORY_SEPARATOR.'index.html');
+        return $this->str_replace($file,$data);
+    }
+
+    /**
+     * 获取配置
+     * @param string $domain
+     * @return string
+     */
+    public function getIndexJs(string $domain):string
+    {
+        $data = [
+            'config'=>'../../normative/home/config',
+        ];
+        $file = file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'template'.DIRECTORY_SEPARATOR.'index.js');
+        return $this->str_replace($file,$data);
+
+    }
+
+    /**
+     * 获取配置
+     * @param string $domain
+     * @return string
+     */
+    public function getConfig(string $domain):string
+    {
+        $data = [
+//            'config'=>'../../normative/home/config',
+        ];
+        $file = file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'template'.DIRECTORY_SEPARATOR.'config.js');
+        return $this->str_replace($file,$data);
+    }
+    /**
+     * 替换
+     * @param $template
+     * @param $data
+     * @return string
+     */
+    protected function str_replace($template, $data):string
+    {
+        foreach($data as $key=>$vuleu){
+            if(!is_array($vuleu)){
+                $template = str_replace("'{{{$key}}}'",$vuleu,$template);
+                $template = str_replace("{{{$key}}}",$vuleu,$template);
+            }
+        }
+        return $template;
+    }
+
+}
