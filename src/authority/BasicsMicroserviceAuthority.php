ba7a17a66d8cda30f1929b217a378bfc40e7fc4a
皮泽培 <848733913@qq.com>
2019-10-22 18:02:53 +0800

    修复一些错误 增加基础类declare(strict_types=1);设置严格模式 完善权限资源对象管理

--- src/authority/BasicsMicroserviceAuthority.php
+++ src/authority/BasicsMicroserviceAuthority.php
@@ -7,7 +7,59 @@
 namespace pizepei\basics\authority;
 
 
+use pizepei\staging\App;
+
 class BasicsMicroserviceAuthority
 {
+    protected $authExtend=[];
+
+    /**
+     * Authority constructor.
+     * @param $pattern
+     * @param App $app
+     */
+    public function __construct($pattern,App $app)
+    {
+        $this->app = $app;
+        # jwt模式
+        $this->pattern = $pattern;
+    }
+    /**
+     * 统一返回
+     * @param $parameter 方法
+     * @return array
+     */
+    public function start($parameter)
+    {
+        if($parameter === 'public'){
+            $this->status = true;
+        }
+        return $this->$parameter();
+    }
+    /**
+     * @Author 皮泽培
+     * @Created 2019/10/22 16:58
+     * @return array [json] 定义输出返回数据
+     * @title  路由标题
+     * @explain 路由功能说明
+     * @throws \Exception
+     */
+    public function initializeData()
+    {
+//        var_dump($this->$parameter());
+
+        # 获取appid  appid只支持path传递
+
+        #通过appid 获取 应用配置
+
+        # 通过应用配置  IP白名单-》接口权限-》签名-》数据解析
+
+        # 进入路由控制器方法内部
+
+        # 正常获取路由配置中的参数
+
+    }
+
+
 
 }
--- src/controller/BasicsMicroservice.php
+++ src/controller/BasicsMicroservice.php
@@ -27,6 +27,7 @@ class BasicsMicroservice extends Controller
      * @Created 2019/10/21 14:24
      * @param Request $Request
      *   path [object] 路径参数
+     *      appid [uuid] 应用appid
      *   get [object] 路径参数
      *   post [object] post参数
      *   rule [object] 数据流参数
@@ -36,11 +37,11 @@ class BasicsMicroservice extends Controller
      * @title  路由标题
      * @explain 路由功能说明
      * @authGroup basics.menu.getMenu:权限分组1,basics.index.menu:权限分组2
-     * @authExtend UserExtend.list:拓展权限
-     * @baseAuth Resource:public
+     * @authExtend MicroserviceAuth.list:拓展权限
+     * @baseAuth MicroserviceAuth:initializeData
      * @resourceType microservice
      * @throws \Exception
-     * @router get  test
+     * @router get  test/:appid[uuid]
      */
     public function test(Request $Request)
     {
