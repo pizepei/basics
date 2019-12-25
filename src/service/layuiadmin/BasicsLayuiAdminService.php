f713fa34c709ca6f8cc3484c61ff6ca4a0b22ee4
皮泽培 <848733913@qq.com>
2019-12-25 16:11:58 +0800

    设置cdn配置

--- src/service/layuiadmin/BasicsLayuiAdminService.php
+++ src/service/layuiadmin/BasicsLayuiAdminService.php
@@ -17,31 +17,22 @@ class BasicsLayuiAdminService
      */
     public function getIndexHtml(string $domain):string
     {
-        if (\Deploy::CDN_URL ===''){
-            $data = [
-                'version'=>(!app()->__EXPLOIT__ && \Deploy::ENVIRONMENT !=='develop')?'1.0.1':date('YmdHis'),
-                'title'=>\Config::PRODUCT_INFO['title'],
-                'index'=>'../../'.\Deploy::MODULE_PREFIX.'/home/index',//index
-                'base'=>'http://cdn.heil.top/resource/dist/',
-                'views'=>'./'.\Deploy::VIEW_RESOURCE_PREFIX.((!app()->__EXPLOIT__ && \Deploy::ENVIRONMENT !=='develop')?'/dist/':'/src/'),
-                'css'=>'https://www.layuicdn.com/layui-v2.5.5/css/layui.css',
-                'js'=>'https://www.layuicdn.com/layui-v2.5.5/layui.js',
-//                'css'=>'./'.\Deploy::VIEW_RESOURCE_PREFIX.'/start/layui/css/layui.css',
-//                'js'=>'./'.\Deploy::VIEW_RESOURCE_PREFIX.'/start/layui/layui.js',
-            ];
+
+        $data = [
+            'version'=>(!app()->__EXPLOIT__ && \Deploy::ENVIRONMENT !=='develop')?'1.0.1':date('YmdHis'),
+            'title'=>\Config::PRODUCT_INFO['title'],
+            'css'=>'https://www.layuicdn.com/layui-v2.5.5/css/layui.css',
+            'js'=>'https://www.layuicdn.com/layui-v2.5.5/layui.js',
+            #'css'=>'./'.\Deploy::VIEW_RESOURCE_PREFIX.'/start/layui/css/layui.css',
+            #'js'=>'./'.\Deploy::VIEW_RESOURCE_PREFIX.'/start/layui/layui.js',
+        ];
+        if (\Deploy::CDN_URL !==''){
+                $data['index']  =   '../../'.\Deploy::MODULE_PREFIX.'/home/index';//index
+                $data['base']   =  'http://cdn.heil.top/resource/dist/';
         }else{
-            $data = [
-                'version'=>(!app()->__EXPLOIT__ && \Deploy::ENVIRONMENT !=='develop')?'1.0.1':date('YmdHis'),
-                'title'=>\Config::PRODUCT_INFO['title'],
-                'index'=>'../../'.\Deploy::MODULE_PREFIX.'/home/index',//index
-                'base'=>'http://cdn.heil.top/resource/dist/',
-                'css'=>'https://www.layuicdn.com/layui-v2.5.5/css/layui.css',
-                'js'=>'https://www.layuicdn.com/layui-v2.5.5/layui.js',
-            ];
+            $data['index']  =   '../../'.\Deploy::MODULE_PREFIX.'/home/index';//index
+            $data['base']   =  './'.\Deploy::VIEW_RESOURCE_PREFIX.((!app()->__EXPLOIT__ && \Deploy::ENVIRONMENT !=='develop')?'/dist/':'/src/');
         }
-
-
-
         $file = file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'template'.DIRECTORY_SEPARATOR.'index.html');
         return $this->str_replace($file,$data);
     }
