0015f3669380df8a22ea0bc05e190e34d11792c3
pi <pizepei@pizepei.com>
2020-04-06 22:16:07 +0800

    用户管理、网站配置模型

--- src/controller/BasicsAccountManage.php
+++ src/controller/BasicsAccountManage.php
@@ -6,6 +6,7 @@
 namespace pizepei\basics\controller;
 
 
+use pizepei\basics\model\account\AccountModel;
 use pizepei\staging\Controller;
 
 class BasicsAccountManage extends Controller
@@ -17,7 +18,7 @@ class BasicsAccountManage extends Controller
         'User'=>'pizepei',
         'title'=>'账号管理控制器',//控制器标题
         'namespace'=>'bases',//门面控制器命名空间
-        'baseAuth'=>'UserAuth:test',//基础权限继承（加命名空间的类名称）
+        'baseAuth'=>'UserAuth:superAdmin',//基础权限继承（加命名空间的类名称）
         'basePath'=>'/account/manage/',//基础路由
         'baseParam'=>'[$Request:pizepei\staging\Request]',//依赖注入对象
     ];
@@ -27,16 +28,32 @@ class BasicsAccountManage extends Controller
      * @Created 2019/11/16 11:50
      * @return array [json] 定义输出返回数据
      *      data [object]
-     *          lsit [objectList]
-     * @title  路由标题
-     * @explain 路由功能说明
-     * @baseAuth Resource:public
+     *          limit [int]
+     *          page [int]
+     *          count [int]
+     *          list [objectList]
+     *              id  [string]
+     *              surname  [string]
+     *              name  [string]
+     *              nickname [string]
+     *              user_name [string]
+     *              email  [string]
+     *              phone [string]
+     *              parent_id [uuid]
+     *              logon_online_count [int]
+     *              type [int]
+     *              status [int]
+     *              creation_time [string]
+     * @title  获取用户列表
+     * @explain 获取用户列表
      * @throws \Exception
-     * @router get
+     * @router get account-list
      */
     public function accountList()
     {
-
+        succeed(AccountModel::table()
+                ->order('creation_time','desc')
+                ->fetchAllPage(),'获取成功');
     }
 
 }
--- src/model/config/SiteConfigModel.php
+++ src/model/config/SiteConfigModel.php
@@ -47,30 +47,55 @@ class SiteConfigModel extends Model
     protected $table_structure_log = [
     ];
     protected $initData = [
-        ['config'=>[
-            'user'=>[
-                /**
-                 * 注册配置
-                 */
-                'register'=>[
-                    'role'=>'0EQD12A2-8824-9943-E8C9-C83E40F360D1',//注册账号默认角色
-                    'status'=>'2',//状态1等待审核、2审核通过3、禁止使用4、保留
+        [
+            'config'=>[
+                'user'=>[
+                    /**
+                     * 注册配置
+                     */
+                    'register'=>[
+                        'role'=>'0EQD12A2-8824-9943-E8C9-C83E40F360D1',//注册账号默认角色
+                        'status'=>'2',//状态1等待审核、2审核通过3、禁止使用4、保留
+                    ],
+                    /**
+                     * 登录配置
+                     */
+                    'login'=>[
+                        'logon_online_count'=>'3',//同时在线数
+                        'password_wrong_count'=>'5',//密码错误数
+                        'password_wrong_lock'=>'10',//密码错误超过限制的锁定时间：分钟
+                        'logon_token_period_pattern'=>'3',//登录token模式1、谨慎（分钟为单位）2、常规（小时为单位）3、方便（天为单位）4、游客（单位分钟没有操作注销）
+                        'logon_token_period_time'=>'2',//登录token有效期对应logon_token_period_pattern
+                        'password_wrong_lock'=>'10',//密码错误超过限制的锁定时间：分钟
+                        'password_wrong_lock'=>'10',//密码错误超过限制的锁定时间：分钟
+                    ],//默认注册账号登录限制
                 ],
-                /**
-                 * 登录配置
-                 */
-                'login'=>[
-                    'logon_online_count'=>'3',//同时在线数
-                    'password_wrong_count'=>'5',//密码错误数
-                    'password_wrong_lock'=>'10',//密码错误超过限制的锁定时间：分钟
-                    'logon_token_period_pattern'=>'3',//登录token模式1、谨慎（分钟为单位）2、常规（小时为单位）3、方便（天为单位）4、游客（单位分钟没有操作注销）
-                    'logon_token_period_time'=>'2',//登录token有效期对应logon_token_period_pattern
-                    'password_wrong_lock'=>'10',//密码错误超过限制的锁定时间：分钟
-                    'password_wrong_lock'=>'10',//密码错误超过限制的锁定时间：分钟
-                ],//默认注册账号登录限制
-            ],
-            ''=>'',
-            ''=>'',
+                'site'=>[
+                    'PRODUCT_INFO'=>[
+                        'meta'     => 'normphp',
+                        'name'     => 'normphp',
+                        'title'    => 'normphp',
+                        'extend'   => [
+                            'homeLoginLay' => '<h2>欢迎来到2.0版的订阅平台</h2><br><br><p>原来已经在ssr.pizepei.com平台注册过的童鞋们只需要使用相同的邮箱注册即可同步关联账号信息</p><br><br>'
+                        ],
+                        'describe' => '一个非常适合团队协作的微型框架'
+                    ]
+                ],
+                'ACCOUNT'=>[
+                    'algo'                        => 1,
+                    'options'                     => [
+                        'cost' => 11
+                    ],
+                    'number_count'                => 20,
+                    'logon_token_salt'            => 'si8934jfk08343*%wew#jj12@99sidjxjc#$lksjd^&*',
+                    'password_regular'            => [
+                        '/^.*(?=.{6,})(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*+=?]).*$/',
+                        '密码至少并且6位且包含大小写字母+特殊字符 !@#$%^&*?+='
+                    ],
+                    'GET_ACCESS_TOKEN_NAME'       => 'access-token',
+                    'HEADERS_ACCESS_TOKEN_NAME'   => 'HTTP_ACCESS_TOKEN',
+                    'user_logon_token_salt_count' => 22
+                ],
         ]],
     ];
 }
